f03c2ff0395790d479d8f590abf900fbfc969de5
pkarasev <p.karasev@psw.ru>
2024-07-23 11:23:54 +0300

    upd

---
+++ api/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+var db *sql.DB
+var err error
+
+func main() {
+	// подключить и прочитать .env
+	if godotenv.Load("../.env") != nil {
+		log.Fatal("Did not exist file ../.env")
+	}
+
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:                 os.Getenv("DBUSER"),
+		Passwd:               os.Getenv("DBPASS"),
+		Net:                  "tcp",
+		Addr:                 os.Getenv("DBHOST"),
+		DBName:               "taris",
+		AllowNativePasswords: true,
+	}
+
+	// Get a database handle.
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("Connected!")
+}
